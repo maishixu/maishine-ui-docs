012108c701b2159d281de11137b57004b99d29a6
maishixu <1269735668@qq.com>
2025-02-22 23:56:00 +0800

    修改base

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "./",
+  base: "/x-element-docs/",
   title: "XElement",
   description: "Vue UI Cmt",
   themeConfig: {
